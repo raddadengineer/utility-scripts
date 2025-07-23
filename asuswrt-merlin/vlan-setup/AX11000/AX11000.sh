8aa22919a8c65e205c303423f8e54eb833b8d1bf
Sampson <51352671+raddadengineer@users.noreply.github.com>
2025-07-23 01:28:51 -0500

    update ax11000 Dynamic & Resilient Edition

--- asuswrt-merlin/vlan-setup/AX11000/AX11000.sh
+++ asuswrt-merlin/vlan-setup/AX11000/AX11000.sh
@@ -27,6 +27,14 @@
 ###################################################################
 #!/bin/sh
 
+# Wait for core radios to initialize
+for iface in wl0 wl1 wl2; do
+  while ! ifconfig "$iface" >/dev/null 2>&1; do
+    logger "VLAN Bridge: Waiting for $iface to initialize..."
+    sleep 2
+  done
+done
+
 # VLAN IDs
 vlan_main=20
 vlan_iot=30
@@ -35,7 +43,9 @@ vlan_gaming=40
 
 # Interfaces
 taggedPort="eth0"
+otherPorts="eth1 eth2 eth3 eth4 eth5"
 
+# SSID to bridge mappings
 mainSSIDs="wl1"
 gamingSSIDs="wl2"
 iotSSIDs="wl0"
@@ -45,94 +55,75 @@ wifi_eth_main="eth7"
 wifi_eth_gaming="eth8"
 wifi_eth_iot="eth6"
 
-otherPorts="eth1 eth2 eth3 eth4 eth5"
-allWiFiAliases="${wifi_eth_main} ${wifi_eth_gaming} ${wifi_eth_iot}"
-allSSIDs="${mainSSIDs} ${gamingSSIDs} ${iotSSIDs} ${guestSSIDs}"
-
-# ----------------------------------------------------
-# 🧼 Clean default bridge
-# ----------------------------------------------------
-for iface in ${taggedPort} ${allSSIDs} ${allWiFiAliases}; do
-  brctl delif br0 $iface
+# Cleanup br0 safely
+for iface in ${taggedPort} ${mainSSIDs} ${gamingSSIDs} ${iotSSIDs} ${guestSSIDs} \
+             ${wifi_eth_main} ${wifi_eth_gaming} ${wifi_eth_iot}; do
+  if ifconfig "$iface" >/dev/null 2>&1; then
+    brctl delif br0 "$iface"
+  fi
 done
 
-# ----------------------------------------------------
-# 🌐 Create VLAN trunk tags
-# ----------------------------------------------------
-ip link add link ${taggedPort} name ${taggedPort}.${vlan_main} type vlan id ${vlan_main}
-ip link add link ${taggedPort} name ${taggedPort}.${vlan_iot} type vlan id ${vlan_iot}
-ip link add link ${taggedPort} name ${taggedPort}.${vlan_guest} type vlan id ${vlan_guest}
-ip link add link ${taggedPort} name ${taggedPort}.${vlan_gaming} type vlan id ${vlan_gaming}
-ip link set ${taggedPort}.${vlan_main} up
-ip link set ${taggedPort}.${vlan_iot} up
-ip link set ${taggedPort}.${vlan_guest} up
-ip link set ${taggedPort}.${vlan_gaming} up
+# Create VLAN subinterfaces
+for vlan in ${vlan_main} ${vlan_iot} ${vlan_guest} ${vlan_gaming}; do
+  ip link add link ${taggedPort} name ${taggedPort}.${vlan} type vlan id ${vlan}
+  ip link set ${taggedPort}.${vlan} up
+done
 
-# ----------------------------------------------------
-# 🏠 br0 - HAL-9000 Main VLAN
-# ----------------------------------------------------
-brctl addif br0 ${taggedPort}.${vlan_main}
-brctl addif br0 ${mainSSIDs}
-brctl addif br0 ${wifi_eth_main}
+# Helper: safe add to bridge
+add_to_bridge() {
+  bridge=$1
+  shift
+  for iface in "$@"; do
+    if ifconfig "$iface" >/dev/null 2>&1; then
+      brctl addif "$bridge" "$iface"
+    fi
+  done
+}
+
+# br0 - HAL-9000 (Main)
+add_to_bridge br0 ${taggedPort}.${vlan_main} ${mainSSIDs} ${wifi_eth_main}
 nvram set lan_ifnames="${otherPorts} ${taggedPort}.${vlan_main} ${mainSSIDs} ${wifi_eth_main}"
 nvram set br0_ifnames="${otherPorts} ${taggedPort}.${vlan_main} ${mainSSIDs} ${wifi_eth_main}"
 nvram set lan_ifname="br0"
 nvram set br0_ifname="br0"
 
-# ----------------------------------------------------
-# 🧪 br1 - HAL-8000 IoT VLAN
-# ----------------------------------------------------
+# br1 - HAL-8000 (IoT)
 brctl addbr br1
-brctl addif br1 ${taggedPort}.${vlan_iot}
-brctl addif br1 ${iotSSIDs}
-brctl addif br1 ${wifi_eth_iot}
+add_to_bridge br1 ${taggedPort}.${vlan_iot} ${iotSSIDs} ${wifi_eth_iot}
 ip link set br1 up
 nvram set lan1_ifnames="${taggedPort}.${vlan_iot} ${iotSSIDs} ${wifi_eth_iot}"
 nvram set br1_ifnames="${taggedPort}.${vlan_iot} ${iotSSIDs} ${wifi_eth_iot}"
 nvram set lan1_ifname="br1"
 nvram set br1_ifname="br1"
 
-# ----------------------------------------------------
-# 🚪 br2 - HAL-Guest VLAN
-# ----------------------------------------------------
+# br2 - HAL-Guest
 brctl addbr br2
-brctl addif br2 ${taggedPort}.${vlan_guest}
-brctl addif br2 ${guestSSIDs}
+add_to_bridge br2 ${taggedPort}.${vlan_guest} ${guestSSIDs}
 ip link set br2 up
 nvram set lan2_ifnames="${guestSSIDs} ${taggedPort}.${vlan_guest}"
 nvram set br2_ifnames="${guestSSIDs} ${taggedPort}.${vlan_guest}"
 nvram set lan2_ifname="br2"
 nvram set br2_ifname="br2"
 
-# ----------------------------------------------------
-# 🎮 br3 - HAL-9001 Gaming VLAN
-# ----------------------------------------------------
+# br3 - HAL-9001 (Gaming)
 brctl addbr br3
-brctl addif br3 ${taggedPort}.${vlan_gaming}
-brctl addif br3 ${gamingSSIDs}
-brctl addif br3 ${wifi_eth_gaming}
+add_to_bridge br3 ${taggedPort}.${vlan_gaming} ${gamingSSIDs} ${wifi_eth_gaming}
 ip link set br3 up
 nvram set lan3_ifnames="${gamingSSIDs} ${wifi_eth_gaming} ${taggedPort}.${vlan_gaming}"
 nvram set br3_ifnames="${gamingSSIDs} ${wifi_eth_gaming} ${taggedPort}.${vlan_gaming}"
 nvram set lan3_ifname="br3"
 nvram set br3_ifname="br3"
 
-# ----------------------------------------------------
-# 🛡️ AP isolation for HAL-Guest
-# ----------------------------------------------------
-if ifconfig "${guestSSIDs}" >/dev/null 2>&1; then
-  nvram set ${guestSSIDs}_ap_isolate=1
-  wl -i ${guestSSIDs} ap_isolate 1
-fi
-
-# ----------------------------------------------------
-# 📡 Declare wireless interfaces
-# ----------------------------------------------------
-nvram set wl_ifnames="wl0 wl1 wl2 wl0.1"
+# Declare wireless interfaces (only ones that exist)
+wl_declare=""
+for iface in wl0 wl1 wl2 wl0.1; do
+  if ifconfig "$iface" >/dev/null 2>&1; then
+    wl_declare="$wl_declare $iface"
+  fi
+done
+nvram set wl_ifnames="$wl_declare"
 nvram commit
 
-# ----------------------------------------------------
-# 🔄 Restart wireless daemon
-# ----------------------------------------------------
+# Restart wireless daemon
 killall eapd 2>/dev/null || true
 eapd
